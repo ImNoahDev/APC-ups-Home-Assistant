5db6d676427eb74330bb361fc23638476ec4fb14
dev-bash <59371081+dev-bash@users.noreply.github.com>
2023-07-05 12:54:55 +0100

    Update ApcStartup.sh

--- ApcStartup.sh
+++ ApcStartup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Define the session name and command to run
-tmux new-session -d -s “apcpy” "pip install flask && python3 /boot/config/plugins/user.scripts/scripts/APC\ ups/apc.py"
+tmux new-session -d -s “APCpy” "pip install flask && python3 /boot/config/plugins/user.scripts/scripts/APC\ ups/apc.py"
 echo "done"
